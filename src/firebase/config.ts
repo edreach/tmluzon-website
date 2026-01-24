8a97b93d717b1de81deec07a47e1ce0b8b56aa36
Edreach Luzon <edreachluzon@gmail.com>
2026-01-24 15:42:25 +0000

    can we save the uploaded file on base64 to firestore?

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-132800311-4745e",
   "appId": "1:259262474289:web:3dcb403be7e1db4be74d10",
+  "storageBucket": "studio-132800311-4745e.appspot.com",
   "apiKey": "AIzaSyBbmF1st7AKwyGWDR7HVFsKcx6PLx24LFE",
   "authDomain": "studio-132800311-4745e.firebaseapp.com",
   "measurementId": "",
