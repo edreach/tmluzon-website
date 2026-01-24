691c5669b0e3a410128cc805c97dceb27f8cb284
Edreach Luzon <edreachluzon@gmail.com>
2026-01-24 09:49:12 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-132800311-4745e",
+  "appId": "1:259262474289:web:3dcb403be7e1db4be74d10",
+  "apiKey": "AIzaSyBbmF1st7AKwyGWDR7HVFsKcx6PLx24LFE",
+  "authDomain": "studio-132800311-4745e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "259262474289"
+};
