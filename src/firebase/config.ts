42090c4099867ac5b09acd214ecbf597d1b90913
Edreach Luzon <edreachluzon@gmail.com>
2026-01-26 12:02:24 +0000

    firestore.googleapis.com/google.firestore.v1.Firestore/Listen/channel?VE

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 // This is your client-side Firebase configuration. It's safe to expose this.
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBbmF1st7AKwyGWDR7HVFsKcx6PLx24LFE",
+  authDomain: "studio-132800311-4745e.firebaseapp.com",
+  projectId: "studio-132800311-4745e",
+  storageBucket: "studio-132800311-4745e.appspot.com",
+  messagingSenderId: "259262474289",
+  appId: "1:259262474289:web:3dcb403be7e1db4be74d10",
 };
